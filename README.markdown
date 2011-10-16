bea99d0c0e59001d45f932133a76fc8e09355bd1
Anders Wold Eldhuset <andereld@samfundet.no>
2011-10-16 21:57:23 +0200

    Removed the TestUnit files; modified the README and config/deploy.rb

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# README
+
+This is the [Ruby on Rails](http://rubyonrails.org) source code for my personal
+website, [eldhuset.org](http://eldhuset.org).
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,14 @@
 require "bundler/capistrano"
 
+# Add RVM's lib directory to the load path.
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+
+# Load RVM's capistrano plugin.    
+require "rvm/capistrano"
+
+set :rvm_ruby_string, '1.9.2-head@www-eldhuset.org'
+set :rvm_type, :user  # Don't use system-wide RVM
+
 set :application, "www.eldhuset.org"
 set :user, "www-eldhuset.org"
 set :use_sudo, false
@@ -7,13 +16,20 @@ server "eldhuset.org", :web, :app, :db, :primary => true
 set :port, 2222
 
 set :scm, :git
-set :repository,  "set your repository location here"
-set :scm_username, "andereld"
+set :repository,  "git@github.com:andereld/eldhuset.org.git"
 set :branch, "master"
-set :deploy_via, :remote_cache
-default_run_options[:pty] = true
+set :scm_user, "andereld"
 ssh_options[:forward_agent] = true
+set :deploy_via, :remote_cache
 
 set :rails_env, "production"
 set :deploy_to, "/home/www-eldhuset.org/app"
 set :normalize_asset_timestamps, false
+
+namespace :deploy do
+  task :symlink_shared do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+after "deploy:update_code", "deploy:symlink_shared"
